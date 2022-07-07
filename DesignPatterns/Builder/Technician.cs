249a435880ac972a42f94f5d6318eec2986a422a
Jakub Faruga <kuba-faruga@o2.pl>
2022-07-07 16:01:46 +0200

    Added Builder pattern implementation in Program.cs + modified Technician.cs

--- DesignPatterns/Builder/Technician.cs
+++ DesignPatterns/Builder/Technician.cs
@@ -1,14 +1,27 @@
-﻿namespace DesignPatterns.Builder;
+﻿using DesignPatterns.FactoryMethod;
+
+namespace DesignPatterns.Builder;
 
 public class Technician
 {
+    private IComputerBuilder _computer = null!;
+
+    public IComputerBuilder Computer
+    {
+        set { _computer = value; }
+    }
     public void BuildMinimalPc()
     {
-        
+        _computer.AddProcessor();
+        _computer.AddMotherboard();
     }
 
     public void BuildPremiumPc()
     {
-        
+        _computer.AddProcessor();
+        _computer.AddMotherboard();
+        _computer.AddGraphicsCard();
+        _computer.AddHardDrive();
+        _computer.AddBluetoothModule();
     }
 }
--- DesignPatterns/Program.cs
+++ DesignPatterns/Program.cs
@@ -2,8 +2,29 @@
 
 using DesignPatterns.FactoryMethod;
 using DesignPatterns.AbstractFactory;
+using DesignPatterns.Builder;
 
 Console.WriteLine("----------Factory Method----------");
 new FactoryMethodClient().Main();
+
 Console.WriteLine("----------Abstract Factory----------");
 new AbstractFactoryClient().Main();
+
+Console.WriteLine("----------Builder----------");
+var technician = new Technician();
+var computerBuilder = new ComputerBuilder();
+technician.Computer = computerBuilder;
+
+Console.WriteLine("Computer with minimal equipment:");
+technician.BuildMinimalPc();
+Console.WriteLine(computerBuilder.GetProduct().ListParts());
+
+Console.WriteLine("Computer with premium equipment:");
+technician.BuildPremiumPc();
+Console.WriteLine(computerBuilder.GetProduct().ListParts());
+
+Console.WriteLine("Custom computer:");
+computerBuilder.AddProcessor();
+computerBuilder.AddMotherboard();
+computerBuilder.AddHardDrive();
+Console.WriteLine(computerBuilder.GetProduct().ListParts());
