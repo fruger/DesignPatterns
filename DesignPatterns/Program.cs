6d1665cad54465da3ff8ae9c0623a1b86c5e1776
Jakub Faruga <kuba-faruga@o2.pl>
2022-07-08 08:53:57 +0200

    Take out code from Program.cs: created BuilderClient.cs + implemented BuilderClient in Program.cs

--- DesignPatterns/Program.cs
+++ DesignPatterns/Program.cs
@@ -11,20 +11,4 @@ Console.WriteLine("----------Abstract Factory----------");
 new AbstractFactoryClient().Main();
 
 Console.WriteLine("----------Builder----------");
-var technician = new Technician();
-var computerBuilder = new ComputerBuilder();
-technician.Computer = computerBuilder;
-
-Console.WriteLine("Computer with minimal equipment:");
-technician.BuildMinimalPc();
-Console.WriteLine(computerBuilder.GetProduct().ListParts());
-
-Console.WriteLine("Computer with premium equipment:");
-technician.BuildPremiumPc();
-Console.WriteLine(computerBuilder.GetProduct().ListParts());
-
-Console.WriteLine("Custom computer:");
-computerBuilder.AddProcessor();
-computerBuilder.AddMotherboard();
-computerBuilder.AddHardDrive();
-Console.WriteLine(computerBuilder.GetProduct().ListParts());
+new BuilderClient().Main();
