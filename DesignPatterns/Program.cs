10631d17101cc92cc14d684b874726a8cbcbf723
Jakub Faruga <kuba-faruga@o2.pl>
2022-07-11 10:18:42 +0200

    Added Decorator pattern implementation in Program.cs

--- DesignPatterns/Program.cs
+++ DesignPatterns/Program.cs
@@ -3,6 +3,7 @@
 using DesignPatterns.FactoryMethod;
 using DesignPatterns.AbstractFactory;
 using DesignPatterns.Builder;
+using DesignPatterns.Decorator;
 
 Console.WriteLine("----------Factory Method----------");
 new FactoryMethodClient().Main();
@@ -12,3 +13,16 @@ new AbstractFactoryClient().Main();
 
 Console.WriteLine("----------Builder----------");
 new BuilderClient().Main();
+
+Console.WriteLine("----------Decorator----------");
+DecoratorClient decoratorClient = new();
+
+var simple = new DecoratorPc();
+decoratorClient.ClientCode(simple);
+Console.WriteLine();
+
+Firewall firewall = new(simple);
+DataEncryption dataEncryption = new(firewall);
+Console.WriteLine("Client: Now I've got a protected Pc:");
+decoratorClient.ClientCode(dataEncryption);
+
