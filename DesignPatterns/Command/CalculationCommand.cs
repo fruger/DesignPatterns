a851d9008bd181a6306b330596833586da8f8e0e
Jakub Faruga <kuba-faruga@o2.pl>
2022-07-11 13:33:00 +0200

    Created concrete commands (Calculation, Paint, Music, WebBrowser)

---
+++ DesignPatterns/Command/CalculationCommand.cs
@@ -0,0 +1,14 @@
+﻿namespace DesignPatterns.Command;
+
+public class CalculationCommand : ICommand
+{
+    public CalculationCommand()
+    {
+        command = "Calculation";
+    }
+    
+    public override void Execute()
+    {
+        Console.WriteLine("Doing some calculations");
+    }
+}
---
+++ DesignPatterns/Command/WebBrowserCommand.cs
@@ -0,0 +1,13 @@
+﻿namespace DesignPatterns.Command;
+public class WebBrowserCommand : ICommand
+{
+    public WebBrowserCommand()
+    {
+        command = "WebBrowser";
+    }
+    
+    public override void Execute()
+    {
+        Console.WriteLine("Opening web browser");
+    }
+}
