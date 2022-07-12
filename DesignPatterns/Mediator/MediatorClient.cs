17e4384fbfbed7572d00a0a7206055cca9a8b65e
Jakub Faruga <kuba-faruga@o2.pl>
2022-07-12 12:25:41 +0200

    Created MediatorClient.cs

---
+++ DesignPatterns/Mediator/MediatorClient.cs
@@ -0,0 +1,19 @@
+﻿namespace DesignPatterns.Mediator;
+
+public class MediatorClient
+{
+    public void Main()
+    {
+        Checkbox checkbox = new();
+        Textbox textbox = new();
+        
+        new ConcreteMediator(checkbox, textbox);
+        Console.WriteLine("Client selects the check box");
+        checkbox.Check();
+
+        Console.WriteLine();
+
+        Console.WriteLine("Client deselects the check box");
+        checkbox.Uncheck();
+    }
+}
