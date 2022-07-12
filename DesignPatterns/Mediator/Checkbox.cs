b69752775b2a0d4fc325113980f4d79ee78910aa
Jakub Faruga <kuba-faruga@o2.pl>
2022-07-12 12:25:24 +0200

    Created BaseComponent.cs, Checkbox.cs, Textbox.cs, ConcreteMediator.cs

---
+++ DesignPatterns/Mediator/Checkbox.cs
@@ -0,0 +1,18 @@
+﻿namespace DesignPatterns.Mediator;
+
+public class Checkbox : BaseComponent
+{
+    public void Check()
+    {
+        Console.WriteLine("Check box is selected");
+        
+        _mediator.Notify(this, "Checked");
+    }
+
+    public void Uncheck()
+    {
+        Console.WriteLine("Check box isn't selected");
+        
+        _mediator.Notify(this, "Unchecked");
+    }
+}
---
+++ DesignPatterns/Mediator/Textbox.cs
@@ -0,0 +1,18 @@
+﻿namespace DesignPatterns.Mediator;
+
+public class Textbox : BaseComponent
+{
+    public void Display()
+    {
+        Console.WriteLine("Text box is displayed");
+        
+        _mediator.Notify(this, "Displayed");
+    }
+
+    public void Hide()
+    {
+        Console.WriteLine("Text box is hidden");
+        
+        _mediator.Notify(this, "Hidden");
+    }
+}
